e194b3571169c9d5519c8c27e4b145fe506cbf39
valts08 <valts.pilans@gmail.com>
2026-04-09 22:44:48 +0300

    feat: added workout controller routes

--- src/routes/workout.ts
+++ src/routes/workout.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express'
+import workoutController from '../controllers/workout.controller.ts'
 
 const router = Router()
 
-// router.get('/', )
-// router.put('/', )
-// router.post('/', )
-// router.delete('/', )
+router.get('/', workoutController.getWorkouts)
+router.get('/:id', workoutController.getWorkoutById)
 
 export default router
