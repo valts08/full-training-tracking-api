7c90dce038069c4ddd8a6009fef2317408de7ae0
valts08 <valts.pilans@gmail.com>
2026-06-16 22:30:21 +0300

    misc: added if statements for error structure for the most likely prisma error types

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,16 +1,36 @@
 import type { Request, Response, NextFunction } from 'express'
 import { ZodError } from 'zod'
+import { PrismaClientInitializationError, PrismaClientKnownRequestError } from '../generated/prisma/internal/prismaNamespace.ts';
 
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 
-    let errors;
+    let error;
     if (err instanceof ZodError) {
-            errors = err.issues.map(i => ({
+            error = err.issues.map(i => ({
             field: i.path.join(''),
             message: i.message
         }))
 
-        return res.status(400).json({ errors })
+        return res.status(400).json({ error })
+    }
+
+    if (err instanceof PrismaClientKnownRequestError) {
+        error = {
+            code: err.code,
+            message: err.message,
+            meta: err.meta
+        }
+
+        return res.status(400).json({ error })
+    }
+
+    if (err instanceof PrismaClientInitializationError) {
+        error = {
+            errorCode: err.errorCode,
+            message: err.message
+        }
+
+        return res.status(400).json({ error })
     }
 
     return res.status(400).json({ error: err.message })
